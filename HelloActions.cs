98cb7349e2d3fc197b7286828fff11a751051ffc
Jay Padmanabhan <jay_padmanabhan@hotmail.com>
2019-12-10 15:55:03 -0800

    ACTION4

--- HelloActions.cs
+++ HelloActions.cs
@@ -12,7 +12,7 @@ namespace Mythical.HelloActions
 {
     public static class HelloActions
     {
-        [FunctionName("HelloActions")]
+        [FunctionName("hacker6helloworld")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
